c907a39889b3b81b5de562b20bb9273eb2a9ded3
repl.it user <replituser@example.com>
2020-10-29 13:01:57 +0000

    zad1

--- zad1.cpp
+++ zad1.cpp
@@ -5,23 +5,16 @@ using namespace std;
 class Wektor2D
 {
 public:
-    Wektor2D(double xx, double yy)
-    {
-        double A = xx;
-        double B = yy;
-    }
-
-    double norm() const { double xxx = A; }
-
     double norm(double x, double y) const { return pow(pow(x, 2) + pow(y, 2), 0.5); }
 };
 
 int main()
 {
     puts("Hello, World!");
-    Wektor2D q, q(3.1,3.9);
+    Wektor2D q;
     double   norma = 0;
     norma          = q.norm(1.2, 1.6);
+
     cout << "norma=" << norma;
 
     return 0;
